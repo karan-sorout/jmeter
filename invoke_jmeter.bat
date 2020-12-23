aea16cabfacc1a3e1786f7d4cba886731bb0b644
Karan Singh <karan.sorout@gmail.com>
2020-12-24 01:25:37 +0200

    jnekin perf report added

--- invoke_jmeter.bat
+++ invoke_jmeter.bat
@@ -2,5 +2,5 @@
 setlocal
 dir
 cd apache-jmeter-5.3\bin
-jmeter -Jjmeter.save.saveservice.output_format=xml -n -t BlazeDemo_SearchFlight.jmx -l xxxx.jtl
+jmeter -Jjmeter.save.saveservice.output_format=xml -n -t BlazeDemo_SearchFlight.jmx -l jmeter_result.jtl
 exit /b
